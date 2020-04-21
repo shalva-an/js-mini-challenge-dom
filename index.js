643c9426f1b55b306aee52e300ed97e3a2b11fe0
shalva-an <s.halvaa26@gmail.com>
2020-04-21 18:25:12 -0400

    done
    .

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
+window.addEventListener('DOMContentLoaded', (event) => {
+    const header = document.querySelector("#header")
+    console.log("Here's your header:", header.innerHTML)
 
+    /***** Deliverable 2 *****/
+    header.style.color = "red"
 
-/***** Deliverable 2 *****/
+    /***** Deliverable 3 *****/
+    let team = PLAYERS
 
+    const renderTeam = team=> {
+        let container = document.querySelector(".player-container")
+        team.forEach(player => {
+            let rendered = renderPlayer(player)
+            container.appendChild(rendered)            
+        });
+    }
 
-/***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+    const renderPlayer = player =>{
+        let div = document.createElement('div')
+        div.class = "player"
+        div.dataset.number = `"${player.number}`
+        div.innerHTML = `
+            <h3> ${player.name} (<em>${player.nickname}</em>) </h3>
+            <img src="${player.photo} alt="${player.name}">
+        `
+        return div
+    }
+    renderTeam(team)
 
+    /***** Deliverable 4 *****/
+    let removePlayer = document.querySelectorAll('div')[5]
+    removePlayer.remove()
 
-/***** Deliverable 4 *****/
+})
